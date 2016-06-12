252f0d80e3835dbbf12edd6ce8334a7b0998890e
Tendai Mutunhire <tmutunhire@gmail.com>
2016-06-12 19:49:28 +0200

    export default Player

--- app/components/player.component.js
+++ app/components/player.component.js
@@ -10,7 +10,21 @@ class Player extends React.Component {
 
     // Return JSX
     return (
-
+      <div className="player">
+        <div className="player__backward">
+          <button onClick={this.props.backward}><i className="fa fa-backward"></i></button>
+        </div>
+        <div className="player__main">
+          <button onClick={this.props.togglePlay}><i className={playPauseClass}</button>
+          <button onClick={this.props.stop}><i className="fa fa-stop"></i></button>
+          <button onClick={this.props.random}><i className="fa fa-random"></i></button>
+        </div>
+        <div className="player__forward">
+          <button onClick={this.props.forward}><i className="fa fa-forward"></i></button>
+        </div>
+      </div>
     )
   }
 }
+
+export default Player
